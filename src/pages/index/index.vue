93cb9678a8871e1ffa7edbe0d78dab3c0b3a3afd
l y <582275558@qq.com>
2020-03-19 10:33:04 +0800

    初始化项目,登录页面的关闭按钮

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,18 +1,13 @@
 <template>
 	<view class="content">
-		<image class="logo" src="/static/logo.png"></image>
-		<view>
-			<text class="title">{{title}}</text>
-		</view>
+		<img src="https://member-dfjr.oss-cn-beijing.aliyuncs.com/member/%E5%85%B3%E9%97%AD.png?Expires=1584584861&OSSAccessKeyId=TMP.3KjYrcXjYzAY3RDvj5M1vu4QvjWwMy8vvvMnR3nFCDC2nsTbLmWzZeajcNN2gCz8FupU8JKUuq8eRhDyFWraXZKk9Gua9q&Signature=A%2BTI3lsyo0Gjd58V9JdE%2BKTKTaU%3D" alt="">
 	</view>
 </template>
 
 <script>
 	export default {
 		data() {
-			return {
-				title: 'Hello'
-			}
+			
 		},
 		onLoad() {
 
@@ -23,20 +18,17 @@
 	}
 </script>
 
-<style>
+<style lang="less">
 	.content {
 		text-align: center;
-		height: 400upx;
+		height: 400rpx;
+		img{
+	   width: 55rpx;
+	   height: 55rpx;
+	   float: left;
+	   margin: 15rpx 0 0 15rpx;
+		}
 	}
 
-	.logo {
-		height: 200upx;
-		width: 200upx;
-		margin-top: 200upx;
-	}
 
-	.title {
-		font-size: 36upx;
-		color: #8f8f94;
-	}
 </style>
